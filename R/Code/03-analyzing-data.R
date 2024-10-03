a68439d85ec93ee0e2fbac5cf442ea1546b47003
Marina Visintini <140360330+m-visintini@users.noreply.github.com>
2024-10-03 11:07:14 -0400

    Finalize reproducibility package

--- R/Code/03-analyzing-data.R
+++ R/Code/03-analyzing-data.R
@@ -2,16 +2,15 @@
 # 03. Data Analysis
 
 # Libraries
-library(haven)
-library(dplyr)
-library(modelsummary)
-library(stargazer)
-library(ggplot2)
-library(tidyr)
+# library(haven)
+# library(dplyr)
+# library(modelsummary)
+# library(stargazer)
+# library(ggplot2)
+# library(tidyr)
 
 # Load data 
 #household level data
-data_path <- "/Users/worldbank/Documents/RRF Course/DataWork/Data/"
 hh_data   <- read_dta(file.path(data_path, "Final/TZA_CCT_analysis.dta"))
 
 # secondary data 
